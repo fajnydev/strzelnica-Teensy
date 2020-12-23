7a494d17cd870d5efc658850894362ee4429943a
Arkadiusz Wrzos <wrzosdev@gmail.com>
2020-12-24 00:43:48 +0100

    losowe pliki w kolko

---
+++ strzelnica.ino
@@ -0,0 +1,117 @@
+ // Simple WAV file player example
+//
+// Three types of output may be used, by configuring the code below.
+//
+//   1: Digital I2S - Normally used with the audio shield:
+//         http://www.pjrc.com/store/teensy3_audio.html
+//
+//   2: Digital S/PDIF - Connect pin 22 to a S/PDIF transmitter
+//         https://www.oshpark.com/shared_projects/KcDBKHta
+//
+//   3: Analog DAC - Connect the DAC pin to an amplified speaker
+//         http://www.pjrc.com/teensy/gui/?info=AudioOutputAnalog
+//
+// To configure the output type, first uncomment one of the three
+// output objects.  If not using the audio shield, comment out
+// the sgtl5000_1 lines in setup(), so it does not wait forever
+// trying to configure the SGTL5000 codec chip.
+//
+// The SD card may connect to different pins, depending on the
+// hardware you are using.  Uncomment or configure the SD card
+// pins to match your hardware.
+//
+// Data files to put on your SD card can be downloaded here:
+//   http://www.pjrc.com/teensy/td_libs_AudioDataFiles.html
+//
+// This example code is in the public domain.
+
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+AudioPlaySdWav           playWav1;
+// Use one of these 3 output types: Digital I2S, Digital S/PDIF, or Analog DAC
+//AudioOutputI2S           audioOutput;
+//AudioOutputSPDIF       audioOutput;
+AudioOutputAnalog      audioOutput;
+AudioConnection          patchCord1(playWav1, 0, audioOutput, 0);
+AudioConnection          patchCord2(playWav1, 1, audioOutput, 1);
+AudioControlSGTL5000     sgtl5000_1;
+
+#define SD_FILES_COUNTER 1077
+int randNumber;
+
+// Use these with the Teensy Audio Shield
+//#define SDCARD_CS_PIN    10
+//#define SDCARD_MOSI_PIN  7
+//#define SDCARD_SCK_PIN   14
+
+// Use these with the Teensy 3.5 & 3.6 SD card
+//#define SDCARD_CS_PIN    BUILTIN_SDCARD
+//#define SDCARD_MOSI_PIN  11  // not actually used
+//#define SDCARD_SCK_PIN   13  // not actually used
+
+// Use these for the SD+Wiz820 or other adaptors
+#define SDCARD_CS_PIN    10
+#define SDCARD_MOSI_PIN  11
+#define SDCARD_SCK_PIN   13
+
+void setup() {
+  Serial.begin(9600);
+
+  // Audio connections require memory to work.  For more
+  // detailed information, see the MemoryAndCpuUsage example
+  AudioMemory(8);
+
+  SPI.setMOSI(SDCARD_MOSI_PIN);
+  SPI.setSCK(SDCARD_SCK_PIN);
+  if (!(SD.begin(SDCARD_CS_PIN))) {
+    // stop here, but print a message repetitively
+    while (1) {
+      Serial.println("Unable to access the SD card");
+      delay(500);
+    }
+  }
+  // if analog input pin 0 is unconnected, random analog
+  // noise will cause the call to randomSeed() to generate
+  // different seed numbers each time the sketch runs.
+  // randomSeed() will then shuffle the random function.
+  randomSeed(analogRead(0));
+}
+
+void playFile(const char *filename)
+{
+  Serial.print("Playing file: ");
+  Serial.println(filename);
+
+  // Start playing the file.  This sketch continues to
+  // run while the file plays.
+  playWav1.play(filename);
+
+  // A brief delay for the library read WAV info
+  delay(5);
+
+  // Simply wait for the file to finish playing.
+  while (playWav1.isPlaying()) {
+    // uncomment these lines if you audio shield
+    // has the optional volume pot soldered
+    //float vol = analogRead(15);
+    //vol = vol / 1024;
+    // sgtl5000_1.volume(vol);
+  }
+}
+
+
+void loop() {
+  //assuming there could be system files like desktop.ini - decreasing by 3
+  randNumber = random(1, SD_FILES_COUNTER-3);
+  char str[128];
+
+  //sprintf(str, "hello %s", "world");
+  snprintf(str, 128, "S%d.WAV",  randNumber);
+  playFile(str);  // filenames are always uppercase 8.3 format
+  delay(500);
+ 
+}
